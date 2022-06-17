9690416287cad51450ae1755a07869fe352a1e6f
mbudak <promaxcoq@gmail.com>
2022-06-17 11:18:55 -0400

    first gif init

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 // Requires
 const express = require('express');
+const { engine } = require('express-handlebars');
+
 
 // Setup
 const app = express()
 
 // Middlewares
+app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
-});
-
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
 
 // Start Server
 const PORT = 3000;
