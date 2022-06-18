cd3010034af2b5f3537d1406ea4423eaf2a35210
mbudak <promaxcoq@gmail.com>
2022-06-17 22:02:54 -0400

    search form added

--- app.js
+++ app.js
@@ -7,17 +7,30 @@ const { engine } = require('express-handlebars');
 const app = express()
 
 // Middlewares
-app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));
-app.set('view engine', 'handlebars');
-
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 
 // Routes
+app.get('/', (req, res) => {
+  console.log(req.query);
+  
+  res.render('home')
+})
+
 app.get('/', (req, res) => {
   // set the url of the gif
   const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
   // render the hello-gif view, passing the gifUrl into the view to be displayed
   res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
 })
 
 // Start Server
